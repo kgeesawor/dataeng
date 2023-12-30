6b13ef6d27f99865f8e50758ace980d9905c14ee
kgeesawor <kushal.geesawor@gmail.com>
2023-12-30 13:02:53 -0500

    changed loads to chunks due to zombie jobs error

--- dags/load_data.py
+++ dags/load_data.py
@@ -15,26 +15,30 @@ dag = DAG(
 )
 
 # Define the task to load data
-def load_data():
-        
-    #load file
-    path = "/opt/airflow/data/instacart-market-basket-analysis/"
-    df = pd.read_csv(path + "products.csv")
+def load_data(filename):
+    chunk_size = 10000  # Set the desired chunk size
     
-    #connect sql engine to postgres
+    # Load file in chunks
+    path = "/opt/airflow/data/instacart-market-basket-analysis/"
+    reader = pd.read_csv(path + filename + ".csv", chunksize=chunk_size)
+    
+    # Connect SQL engine to PostgreSQL
     engine = create_engine('postgresql://airflow:airflow@172.17.0.1:5432/airflow')
 
-    #create out schema if not exists
+    # Create output schema if not exists
     with engine.connect() as connection:
         connection.execute("CREATE SCHEMA IF NOT EXISTS instacart")
 
-    #create table from file
-    df.to_sql('products', engine, if_exists='replace', index=False, schema='instacart')
+    # Create table from chunks
+    for i, chunk in enumerate(reader):
+        table_name = f"{filename}"
+        chunk.to_sql(table_name, engine, if_exists='append', index=False, schema='instacart')
 
 # Create the task
 load_data_task = PythonOperator(
-    task_id='load_data_task',
+    task_id='load_data_train',
     python_callable=load_data,
+    op_kwargs={'filename': 'order_products__train'},
     dag=dag,
 )
 
